1f355243a3a491b0f3974054d51b46d1b55a5156
Kinzamalik <kiinza.malik@gmail.com>
2021-04-28 17:26:59 +0500

    Expense Tracker

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Child from "./child";
+import { TransactionProvider } from "./transContext";
+
+function App() {
+  return (
+    <div className="App">
+      <TransactionProvider>
+      <Child />
+
+      </TransactionProvider>
+    </div>
+  );
+}
+
+export default App;
